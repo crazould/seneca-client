bebc6e824fc4fb6b78df4c0a790a0eae402cc9d7
crazould <53102589+crazould@users.noreply.github.com>
2021-05-13 00:46:18 +0700

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="nav">
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link>
+    </div>
+    <router-view/>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+  
+    <h1>Login Page</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+<style>
+
+</style>
