a16454ec8d61acc688ce2ada357833bbe155f4a1
crazould <53102589+crazould@users.noreply.github.com>
2021-05-15 00:25:13 +0700

    fix styling login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,18 +4,41 @@
       <v-card>
         <v-img src="../assets/bgr.webp" height="100vh">
           <v-layout justify-end>
-            <v-card max-width="380px" width="380px" height="100vh">
-                <v-card-title class="display-1 text-center" width="100%">
+            <v-card
+              max-width="380px"
+              width="380px"
+              height="100vh"
+              :dark="false"
+            >
+              <v-layout
+                column
+                align-center
+                justify-center
+                fill-height
+                class="mx-5"
+              >
+                <v-card-title width="100%" class="text-h2 mb-2">
                   Seneca
                 </v-card-title>
+                <v-card-subtitle>
+                  Laboratory Project Management Tool
+                </v-card-subtitle>
                 <v-card-text>
                   <v-form>
                     <v-text-field
+                      clearable
+                      outlined
+                      dense
                       name="NIM"
                       label="NIM"
                       id="NIM"
+                      type="text"
                     ></v-text-field>
                     <v-text-field
+                      clearable
+                      outlined
+                      dense
+                      type="password"
                       name="Password"
                       label="Password"
                       id="Password"
@@ -24,11 +47,12 @@
                 </v-card-text>
                 <v-card-actions>
                   <v-layout justify-center>
-                    <v-btn @click="login()">
+                    <v-btn @click="login()" outlined tile>
                       Login
                     </v-btn>
                   </v-layout>
                 </v-card-actions>
+              </v-layout>
             </v-card>
           </v-layout>
         </v-img>
