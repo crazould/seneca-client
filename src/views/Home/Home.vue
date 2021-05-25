20660f98d2422ff6264be57e0fe5a669a5d1aed6
crazould <53102589+crazould@users.noreply.github.com>
2021-05-25 16:48:00 +0700

    add transition between page

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -38,7 +38,9 @@
       <v-toolbar-title>Seneca</v-toolbar-title>
     </v-app-bar>
     <v-main>
-      <router-view />
+      <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition>
     </v-main>
   </v-app>
 </template>
