a135a63d47c13ed76d363d66af7e79d983d33712
crazould <53102589+crazould@users.noreply.github.com>
2021-05-25 16:44:15 +0700

    apply theme

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-    <router-view />
+    <v-fade-transition mode="out-in">
+        <router-view />
+    </v-fade-transition>
 </template>
+
+<script>
+  import '@/styles/overrides.sass'
+  export default {
+    name: 'App',
+    metaInfo: {
+      title: 'Seneca',
+      titleTemplate: '%s | Laboratory Project Management Tool',
+      htmlAttrs: { lang: 'en' },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  }
+</script>
