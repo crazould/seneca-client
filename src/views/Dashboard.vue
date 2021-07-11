ba8521c4989866710877f83a69dfb19c4e2dd823
crazould <53102589+crazould@users.noreply.github.com>
2021-07-11 20:39:36 +0700

    add chart template

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,156 @@
 <template>
   <v-container fluid>
-    <h1>Dashboard</h1>
+    <v-row>
+      <v-col cols="12">
+        <v-row>
+          <v-col
+            v-for="(chart, i) in charts"
+            :key="`chart-${i}`"
+            cols="12"
+            md="6"
+            lg="4"
+          >
+            <material-chart-card
+              :color="chart.color"
+              :data="chart.data"
+              :options="chart.options"
+              :responsive-options="chart.responsiveOptions"
+              :title="chart.title"
+              :type="chart.type"
+              class="pb-5"
+            >
+              <!-- <template #subtitle>
+                <div class="font-weight-light text--secondary">
+                  <div v-html="chart.subtitle" />
+                </div>
+              </template>
+
+              <template #actions>
+                <v-icon class="mr-1" small>
+                  mdi-clock-outline
+                </v-icon>
+
+                <span
+                  class="text-caption grey--text font-weight-light"
+                  v-text="chart.time"
+                />
+              </template> -->
+            </material-chart-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import Vue from "vue";
+
+const lineSmooth = Vue.chartist.Interpolation.cardinal({
+  tension: 0
+});
 export default {
   name: "Dashboard",
   components: {},
-  data: () => ({}),
-  mounted() {},
+  data: () => ({
+    charts: [
+      {
+        type: "Line",
+        color: "primary",
+        title: "Completed Task by Day",
+        subtitle: "Last Campaign Performance",
+        time: "updated 10 minutes ago",
+        data: {
+          labels: ["M", "T", "W", "T", "F", "S", "S"],
+          series: [[12, 17, 7, 17, 23, 18, 38]]
+        },
+        options: {
+          axisX: {
+            showGrid: false
+          },
+          low: 0,
+          high: 38 + 10,
+          chartPadding: {
+            top: 0,
+            right: 5,
+            bottom: 0,
+            left: 0
+          }
+        },
+        responsiveOptions: [
+          [
+            "screen and (max-width: 640px)",
+            {
+              seriesBarDistance: 5,
+              axisX: {
+                labelInterpolationFnc: function(value) {
+                  return value[0];
+                }
+              }
+            }
+          ]
+        ]
+      },
+      {
+        type: "Line",
+        color: "success",
+        title: "Completed Task by Month",
+        subtitle:
+          '<i class="mdi mdi-arrow-up green--text"></i><span class="green--text">55%</span>&nbsp;increase in today\'s sales',
+        time: "updated 4 minutes ago",
+        data: {
+          labels: [
+            "Ja",
+            "Fe",
+            "Ma",
+            "Ap",
+            "Mai",
+            "Ju",
+            "Jul",
+            "Au",
+            "Se",
+            "Oc",
+            "No",
+            "De"
+          ],
+          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
+        },
+        options: {
+          lineSmooth,
+          low: 0,
+          high: 895 + 10, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      },
+      {
+        type: "Bar",
+        color: "accent",
+        title: "Overall Productivity",
+        subtitle: "Last Campaign Performance",
+        time: "campaign sent 26 minutes ago",
+        data: {
+          labels: ["Completed", "Others"],
+          series: [[12, 17]]
+        },
+        options: {
+          lineSmooth,
+          low: 0,
+          high: 17 + 10, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      }
+    ]
+  }),
+  mounted() {}
 };
 </script>
